85821bf5e4a8b35e715f34e17ddb0afebbb8d744
TomiMan7 <4tamas.loki@gmail.com>
2019-09-29 15:53:49 +0200

    Tomi-forraskod felkommentezese

--- szoftmod/src/main/java/Controller.java
+++ szoftmod/src/main/java/Controller.java
@@ -6,14 +6,32 @@ import java.io.FileReader;
 
 public class Controller {
 
+
+    /**
+     * Ez a fuggveny olvassa be a JSON-t es iratja ki a megfelelo receptet a TextFieldbe.
+     *
+     * @param kaja a main osztaly egy peldanya, ennek a tagvaltozojaban lesz elerheto az etel receptje
+     * @param kajanev a lenyilo listaban kivalsztott etelnek a neve. Ezzel tudjuk meg melyik receptet kell kiolvani a JSONbol
+     * @throws FileNotFoundException
+     */
     public static void TipusBetoltes(main kaja, String kajanev) throws FileNotFoundException {
+
+        /**
+         * JSON parser
+         * */
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+        /**
+         * JSON tartalmanak beolvasasa
+         * */
         kaja = gson.fromJson(new FileReader("etelek.json"), main.class);
 
         for(int i = 0; i < kaja.eteltipusok.size(); i++)
         {
 
+            /**
+             * A megfelelo etelhez(kajanev) tartozo recept kiolvasasa a kaja.eteltipusok-bol
+             */
             if(kaja.eteltipusok.elementAt(i).toString().contains(kajanev))
             {
                 main.Receptek.setText("");
@@ -22,6 +40,9 @@ public class Controller {
         }
     }
 
+    /**
+     * Egy egyszeru fgv ami "lekezeli" a NullPointerExceptiont abban az esetben ha eteltipus valtas utan meg nincs etelnev kivalsztva
+     */
     public static void semmi()
     {}
 
--- szoftmod/src/main/java/main.java
+++ szoftmod/src/main/java/main.java
@@ -18,33 +18,72 @@ import javafx.stage.Stage;
 
 public class main extends Application implements EventHandler<ActionEvent> {
 
+    /**
+     * A lenyilo listak deklaracioja
+     * */
     static ComboBox Kajatipus = new ComboBox();
     static ComboBox Kajanev = new ComboBox();
 
+    /**
+     * Receptmezo deklaracioja
+     */
     static TextField Receptek = new TextField();
 
+    /**
+     * Cimke deklaracioja
+     */
     static Label label = new Label("Az Ön által választott étel receptje:");
 
+    /**
+     * A JSON beolvasasahoz hasznalt MAP deklaracioja
+     */
     public Vector<Map<String, String>> eteltipusok = new Vector();
 
+    /**
+     * a main osztaly egy peldanyanak letrehozasa
+     */
     public static main kaja = new main();
 
+    /**
+     * Ures konstruktor
+     */
     public main()
     {}
 
     public void start(Stage primaryStage) throws FileNotFoundException {
 
+        /**
+         * Ablak nev megadasa
+         */
         primaryStage.setTitle("Digitális receptválogatás");
+        /**
+         * Atmeretezhetoseg letiltas
+         */
         primaryStage.setResizable(false);
 
         Pane layout = new Pane();
 
+        /**
+         * Ablakmeret megadas
+         */
         Scene scene = new Scene(layout, 800, 600);
+        /**
+         * Panel hozzadasa a scenehez
+         */
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        /**
+         * Lenyilo lista meretnek beallitasa
+         */
         Kajatipus.setPrefSize(300,20);
+        /**
+         * Lenyilo lista kezdo szovegenek beallitasa
+         */
         Kajatipus.setPromptText("Étel tipusok:");
+        /**
+         * Lenyilo lista helyenek megadasa
+         */
         Kajatipus.setLayoutX(30);
         Kajatipus.setLayoutY(10);
 
@@ -53,38 +92,62 @@ public class main extends Application implements EventHandler<ActionEvent> {
         Kajanev.setLayoutX(360);
         Kajanev.setLayoutY(10);
 
+        /**
+         * Recept szovegenek helye a Bal felso sarokba igazitasa
+         */
         Receptek.setAlignment(Pos.TOP_LEFT);
         Receptek.setPrefWidth(10);
         Receptek.setPrefSize(750,450);
         Receptek.setLayoutX(30);
         Receptek.setLayoutY(150);
+        /**
+         * Szovegmezo valtoztathatosaganak letiltasa
+         */
         Receptek.setEditable(false);
         Receptek.setPromptText("Kérem válasszon ételt a recept megjelenítéséhez!");
 
         label.setLayoutY(120);
         label.setLayoutX(30);
 
+        /**
+         * Lenyilo lista feltoltese a tipusokkal
+         */
         Kajatipus.getItems().addAll("Kérem válasszon étel tipust!",
                 "magyaros",
                  "olasz"
         );
 
+        /**
+         * GUI elemek hozzaadas az ablakhoz
+         */
         layout.getChildren().add(Kajatipus);
         layout.getChildren().add(Kajanev);
         layout.getChildren().add(Receptek);
         layout.getChildren().add(label);
 
+        /**
+         * User input kezelesenek megadasa
+         */
         Kajatipus.setOnAction(this);
         Kajanev.setOnAction(this);
     }
 
     @Override
     public void handle(ActionEvent event) {
+        /**
+         * User input helyenek megszerzese
+         */
         if (event.getSource() == Kajatipus) {
 
             if(Kajatipus.getValue() == "magyaros")
             {
+                /**
+                 * Osszes eddigi elem kitorlese a korabbi keresesek miatt
+                 */
                 Kajanev.getItems().clear();
+                /**
+                 * Uj elemekkel valo feltoltes
+                 */
                 Kajanev.getItems().addAll(
                         "gulyas",
                         "porkolt"
